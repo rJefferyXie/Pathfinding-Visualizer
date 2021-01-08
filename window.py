daf108d3615b71fad50b8a6bfa3db05fb51c2384
Jeffery Xie <73203729+rJefferyXie@users.noreply.github.com>
2021-01-08 15:13:11 -0600

    Add files via upload

--- window.py
+++ window.py
@@ -38,7 +38,7 @@ class Window:
         else:
             pygame.draw.rect(self.win, (255, 255, 255), (square_size + 1, square_size * 41 + 11, square_size * 8 - 2, square_size * 2 - 2))
         text = self.font.render("A* Search", True, (0, 0, 0))
-        self.win.blit(text, (square_size * 2 + 10, square_size * 42))
+        self.win.blit(text, (square_size * 3, square_size * 42 + 2))
 
         # Greedy BFS Algorithm
         pygame.draw.rect(self.win, (0, 0, 0), (square_size, square_size * 44, square_size * 8, square_size * 2))
@@ -47,7 +47,7 @@ class Window:
         else:
             pygame.draw.rect(self.win, (255, 255, 255), (square_size + 1, square_size * 44 + 1, square_size * 8 - 2, square_size * 2 - 2))
         text = self.font.render("Greedy Best First", True, (0, 0, 0))
-        self.win.blit(text, (square_size + 2, square_size * 44 + 5))
+        self.win.blit(text, (square_size + 9, square_size * 44 + 7))
 
         # BFS Algorithm
         pygame.draw.rect(self.win, (0, 0, 0), (square_size * 19, square_size * 44, square_size * 8, square_size * 2))
@@ -56,7 +56,7 @@ class Window:
         else:
             pygame.draw.rect(self.win, (255, 255, 255), (square_size * 19 + 1, square_size * 44 + 1, square_size * 8 - 2, square_size * 2 - 2))
         text = self.font.render("Breadth First", True, (0, 0, 0))
-        self.win.blit(text, (square_size * 20 + 4, square_size * 44 + 5))
+        self.win.blit(text, (square_size * 20 + 6, square_size * 44 + 7))
 
         # Dijkstra's Algorithm
         pygame.draw.rect(self.win, (0, 0, 0), (square_size * 10, square_size * 41 + 10, square_size * 8, square_size * 2))
@@ -64,8 +64,8 @@ class Window:
             pygame.draw.rect(self.win, blue2, (square_size * 10 + 1, square_size * 41 + 11, square_size * 8 - 2, square_size * 2 - 2))
         else:
             pygame.draw.rect(self.win, (255, 255, 255), (square_size * 10 + 1, square_size * 41 + 11, square_size * 8 - 2, square_size * 2 - 2))
-        text = self.font.render("Dijkstra's Algo.", True, (0, 0, 0))
-        self.win.blit(text, (square_size * 10 + 10, square_size * 42))
+        text = self.font.render("Dijkstra's Algorithm", True, (0, 0, 0))
+        self.win.blit(text, (square_size * 10 + 2, square_size * 42 + 2))
 
         # DFS Algorithm
         pygame.draw.rect(self.win, (0, 0, 0), (square_size * 19, square_size * 41 + 10, square_size * 8, square_size * 2))
@@ -74,22 +74,22 @@ class Window:
         else:
             pygame.draw.rect(self.win, (255, 255, 255), (square_size * 19 + 1, square_size * 41 + 11, square_size * 8 - 2, square_size * 2 - 2))
         text = self.font.render("Depth First", True, (0, 0, 0))
-        self.win.blit(text, (square_size * 20 + 8, square_size * 42))
+        self.win.blit(text, (square_size * 20 + 12, square_size * 42 + 2))
 
         # Change Speed Button
         pygame.draw.rect(self.win, (0, 0, 0), (square_size * 10, square_size * 44, square_size * 8, square_size * 2))
         pygame.draw.rect(self.win, (255, 255, 255), (square_size * 10 + 1, square_size * 44 + 1, square_size * 8 - 2, square_size * 2 - 2))
         text = self.font.render(self.speed, True, (0, 0, 0))
         if self.speed in ("Fast", "Slow"):
-            self.win.blit(text, (square_size * 13, square_size * 44 + 5))
+            self.win.blit(text, (square_size * 13 + 3, square_size * 44 + 7))
         else:
-            self.win.blit(text, (square_size * 12 + 5, square_size * 44 + 5))
+            self.win.blit(text, (square_size * 12 + 6, square_size * 44 + 7))
     
     def draw_results(self):
         if self.previous_results:
             for i in range(len(self.previous_results)):
                 text = self.font.render(self.previous_results[i], True, (0, 0, 0))
-                self.win.blit(text, (square_size * 27 + 10, square_size * (41 + i) + 10))
+                self.win.blit(text, (square_size * 27 + 10, square_size * (41 + i) + 12))
 
     def draw_solution(self, start, end, path, draw):
         # Total cost (sum of the weights of all nodes from start to end) of path found
